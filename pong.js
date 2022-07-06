80bdc4de5abb64eecac135fedcfc3fcc2d99863b
edumartins1991 <eduardo.martins1991@outlook.com>
2022-07-06 17:45:10 -0300

    criado HTML e CSS-implantada raquete

--- pong.js
+++ pong.js
@@ -1,9 +1,27 @@
+//BallVariables
 let xBallPos = 300;
 let yBallPos = 200;
 let dBall = 15;
 let rBall = dBall / 2;
-let spdXBall = 9;
-let spdYBall = 9;
+let spdXBall = 11;
+let spdYBall = 7;
+//RacketPlayerVariables
+let xRacketPlayerPos = 5;
+let yRacketPlayerPos = 150;
+let wRacketPlayer = 10;
+let hRacketPlayer = 90;
+let spdXRacketPlayer = 11;
+let spdYRacketPlayer = 90;
+//RacketPcVariables
+let xRacketPcPos = 5;
+let yRacketPcPos = -150;
+let wRacketPc = 10;
+let hRacketPc = 90;
+let spdXRacketPc = 11;
+let spdYRacketPc = 90;
+
+
+
 
 function setup() {
   createCanvas(600, 400);
@@ -12,18 +30,30 @@ function setup() {
 function draw() {
   background(0);
   circle (xBallPos,yBallPos,dBall);
-  rect ();
+  rect (xRacketPlayerPos, yRacketPlayerPos, wRacketPlayer, hRacketPlayer);
+  rect (xRacketPcPos, yRacketPcPos, wRacketPc, hRacketPc);
   ballPos();
-  wallCollideDetect();
+  wallBallCollideDetect();
+  racketPlayerMove();
+  wallRacketCollideDetect();
+  racketCollideDetect();
 }
 
 function ballPos(){
   xBallPos += spdXBall;
   yBallPos += spdYBall;
 }
-
-function wallCollideDetect(){
-  if (xBallPos + rBallBall > width || xBallPos - rBall < 0) {
+function racketPlayerMove(){
+  if (keyIsDown(UP_ARROW)){
+    yRacketPlayerPos -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRacketPlayerPos += 10;
+  }
+  
+}
+function wallBallCollideDetect(){
+  if (xBallPos + rBall > width || xBallPos - rBall < 0) {
     spdXBall *= -1;
       
   }
@@ -32,4 +62,26 @@ function wallCollideDetect(){
   spdYBall *= -1;
       
   }
+
+}
+
+function wallRacketCollideDetect(){
+
+  if(yRacketPlayerPos > 310){
+    
+    yRacketPlayerPos = 310;
+     }
+  
+  if(yRacketPlayerPos < 0){
+    
+    yRacketPlayerPos = 0;
+     
+     } 
+}
+
+function racketCollideDetect() {
+    if (xBallPos - rBall < xRacketPlayerPos + wRacketPlayer && yBallPos - rBall < yRacketPlayerPos + hRacketPlayer && yBallPos + rBall > yRacketPlayerPos) {
+      
+        spdXBall *= -1;
+    }
 }
