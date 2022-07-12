edea586e9f45411200f33cbfcbb33be74d073a1f
edumartins1991 <eduardo.martins1991@outlook.com>
2022-07-12 18:14:29 -0300

    adicionado scoreboard

--- pong.js
+++ pong.js
@@ -3,24 +3,24 @@ let xBallPos = 300;
 let yBallPos = 200;
 let dBall = 15;
 let rBall = dBall / 2;
-let spdXBall = 11;
-let spdYBall = 7;
+let spdXBall = 15;
+let spdYBall = 11;
+//Racket weight & widht
+let wRacket = 10;
+let hRacket = 90;
 //RacketPlayerVariables
 let xRacketPlayerPos = 5;
 let yRacketPlayerPos = 150;
-let wRacketPlayer = 10;
-let hRacketPlayer = 90;
 let spdXRacketPlayer = 11;
 let spdYRacketPlayer = 90;
 //RacketPcVariables
-let xRacketPcPos = 5;
-let yRacketPcPos = -150;
-let wRacketPc = 10;
-let hRacketPc = 90;
+let xRacketPcPos = 585;
+let yRacketPcPos = 150;
 let spdXRacketPc = 11;
 let spdYRacketPc = 90;
-
-
+//goal
+let playerGoal = 0;
+let pcGoal = 0;
 
 
 function setup() {
@@ -30,13 +30,23 @@ function setup() {
 function draw() {
   background(0);
   circle (xBallPos,yBallPos,dBall);
-  rect (xRacketPlayerPos, yRacketPlayerPos, wRacketPlayer, hRacketPlayer);
-  rect (xRacketPcPos, yRacketPcPos, wRacketPc, hRacketPc);
+  racketDraw (xRacketPcPos, yRacketPcPos);
+  racketDraw (xRacketPlayerPos, yRacketPlayerPos);
   ballPos();
   wallBallCollideDetect();
   racketPlayerMove();
-  wallRacketCollideDetect();
-  racketCollideDetect();
+  racketPcMove();
+  wallRacketPlayerCollideDetect();
+  wallRacketPcCollideDetect();
+  racketPlayerBallCollideDetect();
+  racketPcBallCollideDetect();
+  scoreBoard ();
+  scoreAdd ();
+
+}
+
+function racketDraw (x,y){
+    rect (x, y, wRacket, hRacket);
 }
 
 function ballPos(){
@@ -55,17 +65,25 @@ function racketPlayerMove(){
 function wallBallCollideDetect(){
   if (xBallPos + rBall > width || xBallPos - rBall < 0) {
     spdXBall *= -1;
-      
+    spdYBall *= 1.02;
+    console.log(spdYBall);
   }
   
   if (yBallPos + rBall > height || yBallPos - rBall < 0) {
   spdYBall *= -1;
-      
+  
+  }
+  if (xBallPos > 600){
+    xBallPos = 300
+  }
+
+  if (xBallPos < 0){
+    xBallPos = 300
   }
 
 }
 
-function wallRacketCollideDetect(){
+function wallRacketPlayerCollideDetect(){
 
   if(yRacketPlayerPos > 310){
     
@@ -78,10 +96,48 @@ function wallRacketCollideDetect(){
      
      } 
 }
+function wallRacketPcCollideDetect(){
+    if(yRacketPcPos > 310){
+    
+        yRacketPcPos = 310;
+         }
+      
+      if(yRacketPcPos < 0){
+        
+        yRacketPcPos = 0;
+         
+         } 
+}
 
-function racketCollideDetect() {
-    if (xBallPos - rBall < xRacketPlayerPos + wRacketPlayer && yBallPos - rBall < yRacketPlayerPos + hRacketPlayer && yBallPos + rBall > yRacketPlayerPos) {
+function racketPlayerBallCollideDetect() {
+    if (xBallPos - rBall < xRacketPlayerPos + wRacket && yBallPos - rBall < yRacketPlayerPos + hRacket
+        && yBallPos + rBall > yRacketPlayerPos) {
       
         spdXBall *= -1;
     }
+}
+function racketPcBallCollideDetect() {
+    if (xBallPos + rBall > xRacketPcPos - wRacket && yBallPos + rBall < yRacketPcPos + hRacket
+        && yBallPos + rBall > yRacketPcPos) {
+      
+        spdXBall *= -1;
+    }
+}
+
+function racketPcMove(){
+    yRacketPcPos = yBallPos-117;
+}
+function scoreBoard(){
+  
+  fill (255)
+  text(playerGoal, 278, 26)
+  text(pcGoal, 321, 26)
+}
+function scoreAdd(){
+  if (xBallPos > 590){
+    playerGoal +=1;
+  }
+  if (xBallPos < 10){
+    pcGoal +=1;
+  }
 }
